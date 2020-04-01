b0f3868812ae35c8c045083a42f6ead1b0a4aa7a
Th3Wall <mandellidavide.95@gmail.com>
2020-04-01 02:16:01 +0200

    added iubenda for policy and cookie policy

--- src/routes/home.js
+++ src/routes/home.js
@@ -56,7 +56,7 @@ export default class Home extends Component {
 					}
 				</div>
 				<div>
-					<p class="my-10 text-center">Developed with ❤️ by <a class="text-orange-500" href={process.env.PREACT_APP_DEV_LINK}>{process.env.PREACT_APP_DEV_NAME}</a></p>
+					<p class="mt-10 mb-8 text-center">Developed with ❤️ by <a class="text-orange-500" href={process.env.PREACT_APP_DEV_LINK}>{process.env.PREACT_APP_DEV_NAME}</a></p>
 				</div>
 			</Fragment>
 		);
